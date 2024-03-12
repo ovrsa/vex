b9b72f6f5ab6c0531b0b2264257e412a906c2e74
ovrsa <92124533+ovrsa@users.noreply.github.com>
2024-03-12 09:26:52 +0900

    バックエンドの環境構築

--- vex_app/src/pages/Home.tsx
+++ vex_app/src/pages/Home.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { scrapeEvents } from '../services/scraper';
+
 
 const Home = () => {
-    const [events, setEvents] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const url = 'https://www.walkerplus.com/top/ar0300/';
-            const scrapedEvents = await scrapeEvents(url);
-            setEvents(scrapedEvents);
-        };
-
-        fetchData();
-    }, []);
 
     return (
         <div className="bg-gray-100 min-h-screen">
             <nav className="bg-white shadow">
                 <div className="container mx-auto px-6 py-3">
-                    <div className="flex justify-between items-center">
+                    <div className="flex justif
+                    y-between items-center">
                         <div className="text-lg font-semibold">Vex</div>
                     </div>
                 </div>
@@ -37,12 +26,7 @@ const Home = () => {
                 <div className="mt-3 text-sm text-gray-500">地図を表示</div>
             </div>
 
-            {events.map((event, index) => (
-                <div key={index}>
-                    <p>{event.eventName}</p>
-                    <p>{event.address}</p>
-                </div>
-            ))}
+
         </div >
     );
 };
