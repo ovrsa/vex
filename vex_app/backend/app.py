8ca078be2cb3017b911911f3f309379935f442da
ovrsa <92124533+ovrsa@users.noreply.github.com>
2024-03-17 22:31:13 +0900

    refactor_app.pyの処理の分割

--- vex_app/backend/app.py
+++ vex_app/backend/app.py
@@ -11,109 +11,152 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
+def fetch_html(url):
+    """
+    指定されたURLからHTMLを取得
+    Args:
+        url (str): 取得するHTMLのURL
+    Returns:
+        bytes: HTMLコンテンツ
+        エラーが発生した場合はNoneを返す
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            return response.read()
+    except Exception as e:
+        logger.error(f'Error fetching the URL: {e}')
+        return None
+
+
+def extract_event_info(html):
+    """
+    HTMLからイベント情報を抽出
+    Args:
+        html (bytes): 解析するHTMLコンテンツ
+    Returns:
+        dict: イベント情報を含む辞書
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='m-mainlist-item')
+    events_dict = {}
+    for item in items:
+        event_uid = str(uuid.uuid4())
+        event_info = parse_event_item(item)
+        events_dict[event_uid] = event_info
+    return events_dict
+
+
+def parse_event_item(item):
+    """
+    個々のイベントアイテムから詳細情報を抽出
+    Args:
+        item (bs4.element.Tag): イベントアイテムを表すBeautifulSoupオブジェクト
+    Returns:
+        dict: イベントの詳細情報
+    """
+    # イベントのリンクと画像のURLを抽出
+    txt_tag = item.find('a', class_='m-mainlist-item__txt')
+    event_link = txt_tag.get('href') if txt_tag else None
+    # イベントの画像のURLを抽出
+    img_tag = item.find('a', class_='m-mainlist-item__img').find('img')
+    img_src = img_tag.get('src') if img_tag else None
+    return {
+        'event_link': event_link,
+        'map_link': 'https://www.walkerplus.com' + event_link + 'map.html' if event_link else None,
+        'price_link': 'https://www.walkerplus.com' + event_link + 'price.html' if event_link else None,
+        'image_src': img_src,
+    }
+
+
+def fetch_event_details(events_dict):
+    """
+    イベントの詳細情報を取得
+    Args:
+        events_dict (dict): イベント情報を含む辞書
+    Returns:
+        None
+    """
+    for uid, event in events_dict.items():
+        if event['map_link']:
+            html = fetch_html(event['map_link'])
+            if html:
+                parse_event_details(html, event)
+        if event['price_link']:
+            html = fetch_html(event['price_link'])
+            if html:
+                parse_price_details(html, event)
+
+
+def parse_event_details(html, event):
+    """
+    イベントの詳細情報をHTMLから抽出
+    Args:
+        html (bytes): 解析するHTMLコンテンツ
+        event (dict): 更新するイベント情報
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    detail_header = soup.find('h1', class_='m-detailheader-heading__ttl')
+    event['event_name'] = detail_header.get_text(
+        strip=True) if detail_header else 'Event name not found'
+    info_table = soup.find('table', class_='m-infotable__table')
+    if info_table:
+        for row in info_table.find_all('tr', class_='m-infotable__row'):
+            th = row.find('th', class_='m-infotable__th')
+            td = row.find('td', class_='m-infotable__td')
+            if th and td:
+                key = th.get_text(strip=True)
+                # <a>タグを探す
+                a_tag = td.find('a')
+                if a_tag and a_tag.has_attr('href'):
+                    # <a>タグが存在し、href属性がある場合、リンクをvalueに設定
+                    value = a_tag['href']
+                else:
+                    # <a>タグがない場合、またはhref属性がない場合、テキストをvalueに設定
+                    value = ' '.join(td.stripped_strings)
+                event[key] = value
+    else:
+        logger.debug('Info table not found')
+
+
+def parse_price_details(html, event):
+    """
+    イベントの価格情報をHTMLから抽出
+    Args:
+        html (bytes): 解析するHTMLコンテンツ
+        event (dict): 更新するイベント情報
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    price_table = soup.find('table', class_='m-infotable__table')
+    if price_table:
+        for row in price_table.find_all('tr', class_='m-infotable__row'):
+            th = row.find('th', class_='m-infotable__th')
+            td = row.find('td', class_='m-infotable__td')
+            if th and td:
+                key = th.get_text(strip=True)
+                a_tag = td.find('a')
+                if a_tag and a_tag.has_attr('href'):
+                    value = a_tag['href']
+                else:
+                    value = ' '.join(td.stripped_strings)
+                event[key] = value
+    else:
+        logger.debug('Price table not found')
+
+
 @app.route('/scrape')
 def scrape():
-    # URLパラメータからURLを取得
+    """
+    クライアントからのリクエストに基づいてウェブスクレイピングを実行し、
+    抽出したイベント情報をJSON形式で返す
+    """
     url = request.args.get('url')
     if not url:
         return 'URL query parameter is missing', 400
-    logger.debug(f'Requested URL: {url}')
-
-    try:
-        # URLを開いてHTMLを取得
-        # url=https://www.walkerplus.com/event_list/0315/ar0313/
-        with urllib.request.urlopen(url) as response:
-            html = response.read()
-            # HTMLを文字列に変換
-    except Exception as e:
-        logger.error(f'Error fetching the URL: {e}')
+    html = fetch_html(url)
+    if html is None:
         return 'Error fetching the URL', 500
-
-    logger.debug(f'HTML snippet: {html[:500]}')
-
-    # 地区のイベント情報を取得
-    soup = BeautifulSoup(html, 'html.parser')
-
-    items = soup.find_all('div', class_='m-mainlist-item')
-
-    # イベント情報を格納するための辞書
-    events_dict = {}
-
-    # イベントの一覧情報を取得
-    for item in items:
-        txt_tag = item.find('a', class_='m-mainlist-item__txt')
-        if txt_tag:
-            event_link = txt_tag.get('href')
-            logger.debug(f'Event link: {event_link}')
-        else:
-            logger.debug('Event text link tag not found')
-
-        # イベントの画像を取得
-        img_tag = item.find('a', class_='m-mainlist-item__img').find('img')
-        img_src = img_tag.get('src') if img_tag else None
-        logger.debug(f'Image source: {img_src}')
-
-        event_uid = str(uuid.uuid4())
-        event_info = {
-            'map_link': 'https://www.walkerplus.com' + event_link + 'map.html',
-            'price_link': 'https://www.walkerplus.com' + event_link + 'price.html',
-            'image_src': img_src,
-        }
-        events_dict[event_uid] = event_info
-
-    # map.htmlからイベントの詳細情報を取得
-    for uid, event in events_dict.items():
-        map_link = event['map_link']
-        logger.debug(f'Fetching_map_link: {map_link}')
-        with urllib.request.urlopen(map_link) as response:
-            html = response.read()
-        soup = BeautifulSoup(html, 'html.parser')
-
-        detail_header = soup.find('h1', class_='m-detailheader-heading__ttl')
-        if detail_header:
-            event_name = detail_header.get_text(strip=True)
-            event['event_name'] = event_name
-            logger.debug(f'Event name: {event_name}')
-        else:
-            logger.debug('Event name not found')
-            event['event_name'] = 'Event name not found'
-
-        info_table = soup.find('table', class_='m-infotable__table')
-        if info_table:
-            for row in info_table.find_all('tr', class_='m-infotable__row'):
-                th = row.find('th', class_='m-infotable__th')
-                td = row.find('td', class_='m-infotable__td')
-                if th and td:
-                    key = th.get_text(strip=True)
-                    value = ' '.join(td.stripped_strings)
-                    event[key] = value
-        else:
-            logger.debug('Info table not found')
-
-        logger.debug(f'Event info: {event}')
-
-    # price.htmlからイベントの料金情報を取得
-    for uid, event in events_dict.items():
-        price_link = event['price_link']
-        logger.debug(f'Fetching_price_link: {price_link}')
-        with urllib.request.urlopen(price_link) as response:
-            html = response.read()
-        soup = BeautifulSoup(html, 'html.parser')
-
-        price_table = soup.find('table', class_='m-infotable__table')
-        if price_table:
-            for row in price_table.find_all('tr', class_='m-infotable__row'):
-                th = row.find('th', class_='m-infotable__th')
-                td = row.find('td', class_='m-infotable__td')
-                if th and td:
-                    key = th.get_text(strip=True)
-                    value = ' '.join(td.stripped_strings)
-                    event[key] = value
-        else:
-            logger.debug('Price table not found')
-
-        logger.debug(f'Event info: {event}')
-
+    events_dict = extract_event_info(html)
+    fetch_event_details(events_dict)
     logger.debug(f'Events: {json.dumps(events_dict, ensure_ascii=False)}')
     return json.dumps(events_dict, ensure_ascii=False)
 
