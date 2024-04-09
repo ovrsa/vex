693fc05ecb14ebdcbe03f71a3ed082f06466dc59
ovrsa <sufferin.in@gmail.com>
2024-04-09 22:11:45 +0900

    feat/PRI-85 Create login and Signup screens

--- vex_app/frontend/src/App.tsx
+++ vex_app/frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
+import LogIn from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<LogIn />} />
       </Routes>
     </Router>
 
